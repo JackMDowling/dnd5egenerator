5672e161a432e155ed15d93b2e0be0b9ab85b950
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-12-04 12:23:19 -0700

    more tweaks

--- client/src/SavedChar.jsx
+++ client/src/SavedChar.jsx
@@ -13,9 +13,9 @@ const SavedChar = () => {
         return (
             <div>
             <h3>{c.name}</h3>
-            <h5>{c.gender}    {c.race}    {c.class}</h5>
-            <h6>Str: {c.str}   Dex: {c.dex} Con:   {c.con}</h6>
-            <h6>Int: {c.int}   Wis: {c.wis}   Cha: {c.cha}</h6>
+            <h4>{c.gender}    {c.race}    {c.class}</h4>
+            <h5>Str: {c.str}   Dex: {c.dex} Con:   {c.con}</h5>
+            <h5>Int: {c.int}   Wis: {c.wis}   Cha: {c.cha}</h5>
             <h5>Bond: {c.bond}</h5>
             <h5>Ideal: {c.ideal}</h5>
             <h5>Flaw: {c.flaw}</h5>
